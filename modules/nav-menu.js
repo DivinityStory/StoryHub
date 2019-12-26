08fc29bb4d2550b66aa29b9dfc95ad1f51080c5c
Divinity Story <divinitystory@gmail.com>
2019-12-26 12:13:09 -0500

    moved nav menu code into a javascript file
    Now, to edit the nav menu, just go into the module js for it and edit the code there; the changes will ripple to all pages that use it

---
+++ modules/nav-menu.js
@@ -0,0 +1,31 @@
+"use strict";
+
+//#region HTML to Write
+/*
+`
+<ul class="button-list">
+<li>
+    <a class="nav-link" id="nav-selected" href="#">
+        Home / About
+    </a>
+</li>
+<li>
+    <a class="nav-link" href="table-of-contents.html">
+        Table of Contents
+    </a>
+</li>
+<li>
+    <a class="nav-link" href="#">
+        Latest Chapter
+    </a>
+</li>
+</ul>
+<hr class="thin-hr">
+`
+*/
+//#endregion
+
+// Babel Transpiler Result: https://babeljs.io/repl
+document.write(
+    "\n<ul class=\"button-list\">\n<li>\n    <a class=\"nav-link\" id=\"nav-selected\" href=\"#\">\n        Home / About\n    </a>\n</li>\n<li>\n    <a class=\"nav-link\" href=\"table-of-contents.html\">\n        Table of Contents\n    </a>\n</li>\n<li>\n    <a class=\"nav-link\" href=\"#\">\n        Latest Chapter\n    </a>\n</li>\n</ul>\n<hr class=\"thin-hr\">\n"
+);
