183026dbcdb3425650ee041a2fe1407749aeb102
Divinity Story <divinitystory@gmail.com>
2019-12-27 18:28:44 -0500

    Started work on "to top" and night / day mode buttons

---
+++ js/buttons-module.js
@@ -0,0 +1,17 @@
+"use strict";
+
+//#region HTML to Write
+let buttonHtml =
+    `
+    <div id="mode-button" class="button">
+        <img src="media/sun-icon.png" alt="">
+    </div>
+    <div id="top-button" class="button">
+        <img src="media/top-icon.png" alt="">
+    </div>
+    `;
+//#endregion
+
+// Write the html that was set above
+document.write(buttonHtml);
+
