86ce758c11f8e77d4c6c212cb7ee3e653176029d
Divinity Story <divinitystory@gmail.com>
2019-12-27 19:55:00 -0500

    Finished button functionality and added accompanying styles

--- js/buttons-module.js
+++ js/buttons-module.js
@@ -15,3 +15,32 @@ let buttonHtml =
 // Write the html that was set above
 document.write(buttonHtml);
 
+// Set up some functions to fire when the buttons are clicked.
+document.querySelector("#mode-button").addEventListener("click", swapMode);
+document.querySelector("#top-button").addEventListener("click", backToTop);
+
+// Swap between dark mode and light mode.
+function swapMode() {
+    // Get the content element, our embedded doc.
+    let emDoc = document.querySelector(".embedded-doc");
+
+    // Get the button's image as well, so we can swap it.
+    let modeButton = document.querySelector("#mode-button img");
+
+    // If the doc has an id, then it's in light mode. Otherwise, it's in dark mode.
+    // Swaps the image to the opposite of the mode, to indicate that you click on the button to switch.
+    if (emDoc.id) {
+        emDoc.id = "";
+        modeButton.src = "media/sun-icon.png";
+    }
+    else {
+        emDoc.id = "light-mode";
+        modeButton.src = "media/moon-icon.png";
+    }
+}
+
+// Moves the user back to the top of the document.
+function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
