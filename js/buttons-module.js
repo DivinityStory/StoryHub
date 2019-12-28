8672d33d0bdefaf22ea9cfbc108c136a4f63cd21
Divinity Story <divinitystory@gmail.com>
2019-12-27 22:33:33 -0500

    Added bookmark buttons

--- js/buttons-module.js
+++ js/buttons-module.js
@@ -6,8 +6,17 @@ let buttonHtml =
     <div id="mode-button" class="button">
         <img src="media/sun-icon.png" alt="Toggle Light Mode">
     </div>
+    
     <div id="top-button" class="button">
         <img src="media/top-icon.png" alt="To Top">
+    </div>
+
+    <div id="bookmark-button" class="button">
+        <img src="media/bookmark-icon.png" alt="Save Bookmark">
+    </div>
+
+    <div id="to-bookmark-button" class="button">
+        <img src="media/to-bookmark-icon.png" alt="Go To Bookmark">
     </div>
     `;
 //#endregion
@@ -18,6 +27,8 @@ document.write(buttonHtml);
 // Set up some functions to fire when the buttons are clicked.
 document.querySelector("#mode-button").addEventListener("click", swapMode);
 document.querySelector("#top-button").addEventListener("click", backToTop);
+document.querySelector("#bookmark-button").addEventListener("click", saveBookmark);
+document.querySelector("#to-bookmark-button").addEventListener("click", goToBookmark);
 
 // Swap between dark mode and light mode.
 function swapMode() {
@@ -45,4 +56,30 @@ function swapMode() {
 function backToTop() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
+}
+
+// Save the user's location on the page in localStorage.
+function saveBookmark() {
+    // Get the user's position. If body.scrollTop is 0 (invalid), check documentElement.scrollTop.
+    // Even if the scrollTop actually was 0, it'll still be set to 0.
+    let userPos = document.body.scrollTop ? document.body.scrollTop : document.documentElement.scrollTop;
+
+    // Save it with a key unique to this chapter.
+    localStorage.setItem(`divinity-${document.body.id}-bm`, JSON.stringify(userPos));
+}
+
+// Go to the bookmark stored in local storage. Does nothing if not found.
+function goToBookmark() {
+    // Get the bookmark position for this chapter.
+    let bookmarkPos = localStorage.getItem(`divinity-${document.body.id}-bm`);
+
+    // If it's not found, don't bother.
+    if (bookmarkPos) {
+        // If it was found, parse it.
+        bookmarkPos = JSON.parse(bookmarkPos);
+
+        // Now set the scrolltop accordingly.
+        document.body.scrollTop = bookmarkPos;
+        document.documentElement.scrollTop = bookmarkPos;
+    }
 }
